ba4ad9c9d41d0326bfb43bc96ec31103d25b9174
jsb12302 <jsb12302@naver.com>
2021-11-23 15:17:24 +0900

    주석제거

--- ros/robot_state_machine.py
+++ ros/robot_state_machine.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
 
     DrivingMachine=StateMachine(outcomes=['success'])
     with DrivingMachine:
-        #StateMachine.add('BLOCKING_BAR', BlockingBarState(), transitions={'success': 'LINETRACE'})
-        #StateMachine.add('LINETRACE',LineTrace(),transitions={'success': 'STOP_LINE2_SECTION'})
-        #StateMachine.add('STOP_LINE2_SECTION', StopLine2_section(), transitions={'success': 'GO_7SEC'})
-        #StateMachine.add('GO_7SEC', Go_7sec_class(), transitions={'success': 'S_course'})
+        StateMachine.add('BLOCKING_BAR', BlockingBarState(), transitions={'success': 'LINETRACE'})
+        StateMachine.add('LINETRACE',LineTrace(),transitions={'success': 'STOP_LINE2_SECTION'})
+        StateMachine.add('STOP_LINE2_SECTION', StopLine2_section(), transitions={'success': 'GO_7SEC'})
+        StateMachine.add('GO_7SEC', Go_7sec_class(), transitions={'success': 'S_course'})
         StateMachine.add('S_course', S_course(), transitions={'success': 'T_course'})
         StateMachine.add('T_course', T_course(), transitions={'success': 'success'})
     DrivingMachine.execute()
